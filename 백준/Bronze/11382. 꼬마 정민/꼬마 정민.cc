7da972062ffcec3f7d9c6d19d40eca0a7e2d9f80
Bumju Kim <81178862+KIMBUMJU@users.noreply.github.com>
2024-07-16 14:08:48 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+int main(){
+    
+    long long A, B, C, res;
+    scanf("%lld %lld %lld", &A, &B, &C);
+    res = A+B+C;
+    printf("%lld", res);
+    
+    
+    
+    
+    
+    return 0;
+}
